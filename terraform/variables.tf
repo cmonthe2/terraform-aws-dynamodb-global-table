de0c1d942b399a24501cd6d930d76b6049eaca2d
cameron monthe <cameronmonthe@camerons-MacBook-Pro.local>
2025-12-09 14:00:46 -0500

    feat:inital push

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,12 @@
 variable "table_name" {
   description = "The name of the DynamoDB table. "
   type        = string
+  default     = "global-table-example"
 }
 variable "hash_key" {
   description = "The hash key of the DynamoDB table."
   type        = string
+  default     = "id"
 }
 variable "hash_key_type" {
   description = "The type of the hash key of the DynamoDB table."
@@ -20,7 +22,8 @@ variable "read_capacity" {
   default = 5
 }
 variable "destination_region" {
-  type = string
+  type    = string
+  default = "us-west-2"
 }
 variable "stream_view_type" {
   type    = string
